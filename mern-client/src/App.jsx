fcb70117541678688cb40d992e0c0ed11ae3b462
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-06-09 00:12:53 +0300

    client part (start position)

---
+++ mern-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav>Site Logo</nav>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
