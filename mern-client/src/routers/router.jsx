d95743fd422b387cbf357b7bfabbf51cd71182ba
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-06-09 20:26:02 +0300

    add shop, blog and about routes

--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -4,6 +4,9 @@ import{
 } from "react-router-dom";
 import App from "../App"
 import { Home } from "../home/Home";
+import { Shop } from "../shop/Shop";
+import { About } from "../components/About";
+import { Blog } from "../components/Blog";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -12,6 +15,18 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
+            },
+            {
+                path: "/shop",
+                element: <Shop/>
+            },
+            {
+                path: "/about",
+                element: <About/>
+            },
+            {
+                path: "/blog",
+                element: <Blog/>
             }
         ]
     },
