b37a44b717c4688ae06ec61369518252f7b5be75
Feevicun <121824657+Feevicun@users.noreply.github.com>
2024-06-09 23:24:31 +0300

    add banner and favourite component

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Banner } from '../components/Banner'
+import { FavouriteBooks } from './FavouriteBooks'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Banner/>
+      <FavouriteBooks/>
+    </div>
   )
 }
 
